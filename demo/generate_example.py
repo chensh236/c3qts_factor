30f194be215b35823a0db5355bfbbaad35cdfc7f
freychan <11597727+freychan@user.noreply.gitee.com>
2022-12-23 10:47:26 +0800

    fix demo

--- demo/generate_example.py
+++ demo/generate_example.py
@@ -14,7 +14,7 @@ test_factor_system = FactorSystem(test_factor_class)
 # test_factor_system.parallel_generate(['rb', 'ag'], n_threads=10)
 db = LocaldbDatabase()
 
-test_factor_system.generate(instrument='AG1801', write=True, append=False)
+test_factor_system.generate(instrument='AG1801', begin_datetime='19000101', end_datetime='21000101', write=True, append=False)
 data, ts, _ = db.load_tick_data(symbol='AG1801',
                         symbol_type=ContractType.MERGE_ORI,
                         start='19000101000000000',
@@ -30,4 +30,4 @@ data, ts, _ = db.load_tick_data(symbol='AG1801',
                         factor_name = 'a1_cyh')
 print(data, ts, len(data))
 
-test_factor_system.parallel_generate(products=['AG'], n_threads=2)
+test_factor_system.parallel_generate(products=['AG'], begin_datetime='19000101', end_datetime='21000101',  append=False, n_threads=2)
